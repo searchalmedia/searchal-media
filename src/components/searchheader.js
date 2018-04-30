09781720350c5d004645f878c2cf121b265a247e
searchalmedia <searchalmedia@gmail.com>
2018-04-30 12:36:23 -0600

    changed logo

--- src/components/searchheader.js
+++ src/components/searchheader.js
@@ -11,7 +11,6 @@ class SearchHeader extends Component {
             <div>
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Searchal Media</h1>
                 </header>
             </div>
         );
